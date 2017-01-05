ea56bbd8c3afcda047de4c56c166784975763317
tntkid22 <tntkid222@yahoo.com>
2017-01-04 21:25:18 -0500

    Config Update

--- static/config.js
+++ static/config.js
@@ -2,29 +2,29 @@
 // ID of the Google spreadsheet
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
-var spreadsheetId = "1P9wMb9e0YbhcOua9RQeRCJjllf_L77uV-7i4Q0Yor0o";
+var spreadsheetId = "149KsqD1I0A9O41arOMUpr7e3vWa4-BjoSj2hZvMD4fY";
 
 // ***********************************************************************************
 // Position of Breedables, Shinies and Wants worksheet
 // ***********************************************************************************
 // If you don't have one worksheet, set the value to -1 to disable it
 var breedablesWorksheet = 1;
-var shiniesWorksheet = 2;
-var wantsWorksheet = 3;
+var shiniesWorksheet = -1;
+var wantsWorksheet = -1;
 
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
-var friendCode = "4597-0550-9417";
-var inGameName = "Richie";
+var friendCode = "3454-1315-0697";
+var inGameName = "Matt Wilson";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "https://www.reddit.com/user/tntkid22/";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/3bad8140-aee1-11e6-8169-06598ddd6773.png";
